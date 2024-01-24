fff2f2364371d591249ef7cc65883e8442ba7ec7
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2024-01-23 20:35:12 -0800

    Challenge 4 complete

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -65,11 +65,17 @@ Use `forEach` to build a new array of strings, each string modified by the callb
 ------------------------------------------------------------------------------------------------ */
 
 const greeting = (word) => {
-  // Solution code here...
+  return word.toUpperCase() + "!";
 };
 
 const speaker = (words, callback) => {
-  // Solution code here...
+  let newWords = [];
+
+  words.forEach(element => {
+    newWords.push(callback(element));
+  });
+
+  return newWords;
 };
 
 /* ------------------------------------------------------------------------------------------------
