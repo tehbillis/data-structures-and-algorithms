4e0d99c0a4e1657d804e19f0b0598f2015431450
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2024-03-04 11:03:25 -0800

    Complete challenge 3

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -46,7 +46,7 @@ Write a function named addPurchases that, given an array of objects as input, us
 ------------------------------------------------------------------------------------------------ */
 
 const addPurchases = (arr) => {
-  // Solution code here...
+  return arr.reduce((total, purchase) => { return total + purchase.purchasePrice; }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
