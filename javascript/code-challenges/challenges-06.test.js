e07062abcd290f3ec07884d14db95b60046d78c9
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2024-03-04 11:08:08 -0800

    Complete challenge 4

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -58,7 +58,7 @@ Note: You may not use the array's built-in length property.
 ------------------------------------------------------------------------------------------------ */
 
 const countNumberOfElements = (arr) => {
-  // Solution code here...
+  return arr.reduce((length) => { return length+1; }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
